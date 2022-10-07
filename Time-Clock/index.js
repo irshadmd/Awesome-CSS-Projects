ce6e5c84b40271b762e105d0bed02224a329305c
Sayyad Azhar <80194496+Azhar221@users.noreply.github.com>
2022-10-08 01:19:39 +0530

    Added Time-Clock

---
+++ Time-Clock/index.js
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+=======
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+>>>>>>> e37c9ad82cba9ed44a508c1d0951e73d5f611027
+}, 1000);
