d230e27533f4ecb7bb6c210d7b73575966c5c995
syedmdsaifihassan <syedsaifihassan@gmail.com>
2025-05-09 00:59:44 +0530

    update package

--- emoji-rotator/app/layout.tsx
+++ emoji-rotator/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Emoji Rotator',
+  description: 'Created with ♥️',
+  generator: 'emojiRotator.dev',
 }
 
 export default function RootLayout({
